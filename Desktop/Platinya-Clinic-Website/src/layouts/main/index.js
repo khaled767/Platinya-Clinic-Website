6a6cdf77923f80d1cf1b009f39ec22b602f6f746
Khaled <https://github.com/khaled767>
2026-07-18 00:35:28 +0300

    adding layout to control in pages

---
+++ Desktop/Platinya-Clinic-Website/src/layouts/main/index.js
@@ -0,0 +1,15 @@
+export default function mainLayout(pageContent) {
+  return `
+    <header>
+      <!-- Header Placeholder -->
+    </header>
+
+    <main>
+      ${pageContent}
+    </main>
+
+    <footer>
+      <!-- Footer Placeholder -->
+    </footer>
+  `;
+}
